9d130f6b035f405936d0fc0c6de19e93aeb8ac41
brettkanker <stanisic.luka44@gmail.com>
2023-03-01 16:46:30 +0100

    changed todo cards

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,50 +1,30 @@
 <template>
   <div class="todo">
-    <div class="card" style="width: 18rem;">
+    <div class="card" style="width: 13rem;">
       <div class="card-body">
         <h5 class="card-title">Zähne waschen</h5>
         <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum eum labore aspernatur explicabo.</p>
-        <p class="card-text">Zuhause</p>
+        <a href="#" class="card-link">Zuhause</a>
         <p class="card-text">20:30</p>
         <p class="card-text">Heiss</p>
       </div>
     </div>
 
-    <div class="card" style="width: 18rem;">
+    <div class="card" style="width: 13rem;">
       <div class="card-body">
         <h5 class="card-title">Zähne waschen</h5>
         <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum eum labore aspernatur explicabo.</p>
-        <p class="card-text">Zuhause</p>
+        <a href="#" class="card-link">Zuhause</a>
         <p class="card-text">20:30</p>
         <p class="card-text">Heiss</p>
       </div>
     </div>
 
-    <div class="card" style="width: 18rem;">
+    <div class="card" style="width: 13rem;">
       <div class="card-body">
         <h5 class="card-title">Zähne waschen</h5>
         <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum eum labore aspernatur explicabo.</p>
-        <p class="card-text">Zuhause</p>
-        <p class="card-text">20:30</p>
-        <p class="card-text">Heiss</p>
-      </div>
-    </div>
-
-    <div class="card" style="width: 18rem;">
-      <div class="card-body">
-        <h5 class="card-title">Zähne waschen</h5>
-        <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum eum labore aspernatur explicabo.</p>
-        <p class="card-text">Zuhause</p>
-        <p class="card-text">20:30</p>
-        <p class="card-text">Heiss</p>
-      </div>
-    </div>
-
-    <div class="card" style="width: 18rem;">
-      <div class="card-body">
-        <h5 class="card-title">Zähne waschen</h5>
-        <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum eum labore aspernatur explicabo.</p>
-        <p class="card-text">Zuhause</p>
+        <a href="#" class="card-link">Zuhause</a>
         <p class="card-text">20:30</p>
         <p class="card-text">Heiss</p>
       </div>
@@ -54,6 +34,9 @@
   </div>
 </template>
 
-<style>
+<style scoped>
 
+  .card {
+    margin: 50px 50px 20px 75px
+  }
 </style>
