83333b42159de4812e559fcfe93d4b065b7dd888
brettkanker <stanisic.luka44@gmail.com>
2023-03-22 20:40:02 +0100

    title and desc inputs look better

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -19,9 +19,9 @@ export default {
       this.$emit('edit-task', todo)
     },
     
-    edit_desc(todo, newdesc){
+    edit_task_desc(todo, newDesc){
       //Task Bearbeiten mit neuem Wert
-      todo.desc = newTdecs;
+      todo.description = newDesc;
       //Task in DB Updaten
       this.$emit('edit-task', todo)
     }
@@ -34,30 +34,34 @@ export default {
 
 <template>
   <div class="container d-flex flex-wrap justify-content-left">
-    <div class="card child" v-for="todo in todos">
+    <div class="card" v-for="todo in todos">
       <div class="card-body" :class="todo.completed == true ? 'card-open' : 'card-done'">
-        <input class="card-title title overflow-hidden border-0 bg-transparent" @change="edit_task_title(todo, $event.target.value)" :value="todo.title">
 
-        <p class="card-text description overflow-hidden">{{ todo.description }}</p>
+        
+        <input class="form-control border-0 bg-transparent text-truncate" type="text" placeholder="Titel" style="max-width: 100%;" @change="edit_task_title(todo, $event.target.value)" :value="todo.title">
+    
+        <textarea class="form-control border-0 bg-transparent" type="text" placeholder="Beschreibung" style="max-width: 100%;" @change="edit_task_desc(todo, $event.target.value)" :value="todo.description"> </textarea>
+        
+        
         <p class="card-text category">{{ todo.category }}</p>
         <p class="card-text time">{{ todo.time }}</p>
         <p class="card-text priority">{{ todo.priority == "Hoch" ? '🔴' : (todo.priority == "Mittel" ? '🟡' : '🟢') }}</p>
 
 
         <p :class="todo.completed == true ? 'done' : 'open'">{{ todo.completed == true ? 'Erledigt!' : 'Offen' }}</p>
-        <button class="btn btn-sm btn-outline-primary" @click="toggle_todo_state(todo.id);">{{ todo.completed == true ?
-          'Offen markieren' : 'Erledigt markieren' }}</button>
+        <button class="btn btn-sm btn-outline-primary" @click="toggle_todo_state(todo.id);">{{ todo.completed == true ? 'Offen markieren' : 'Erledigt markieren' }}</button>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+
 .done {
   color: green;
   position: absolute;
   bottom: 40px;
-  left: 10px;
+  left: 15px;
 
 }
 
@@ -65,9 +69,8 @@ export default {
   color: red;
   position: absolute;
   bottom: 40px;
-  left: 10px;
+  left: 15px;
 }
-
 .card {
   margin: 20px 10px 20px 10px;
   width: 13rem;
@@ -81,8 +84,6 @@ export default {
 .title {
   position: absolute;
   top: 15px;
-  left: 10px;
-  height: 50px;
 }
 
 .description {
@@ -90,6 +91,7 @@ export default {
   top: 70px;
   left: 10px;
   height: 100px;
+  width: 100%;
 }
 
 .category {
@@ -109,7 +111,6 @@ export default {
   top: 200px;
   left: 10px;
 }
-
 
 .btn {
   position: absolute;
